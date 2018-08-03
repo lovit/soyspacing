a1fd352244b1ac940cc52cd986bb6be03a1a13c0
lovit <soy.lovit@gmail.com>
2018-08-04 04:17:54 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
-from description import __version__, __author__
+import soyspacing
+import setuptools
 from setuptools import setup, find_packages
 
-readme = """Spacing error correction algorithms. """
+with open('README.md', encoding='utf-8') as fh:
+    long_description = fh.read()
 
-setup(
-   name="soyspacing",
-   version=__version__,
-   author=__author__,
-   author_email='soy.lovit@gmail.com',
-   url='https://github.com/lovit/soyspacing',
-   description="Spacing Error Correction Tools",
-   long_description=readme,
-   install_requires=["numpy>=1.12.0"],
-   keywords = ['Korean spacing error correction'],
-   packages=find_packages()
+setuptools.setup(
+    name="soyspacing",
+    version=soyspacing.__version__,
+    author=soyspacing.__author__,
+    author_email='soy.lovit@gmail.com',
+    description="Spacing Error Correction Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lovit/soyspacing',
+    packages=setuptools.find_packages(),
+    install_requires=["numpy>=1.12.0"],
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    keywords = ['Space error correction', 'Korean spacing', 'Korean', 'Natural Language Processing'],
 )
